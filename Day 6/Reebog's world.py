c9a10b7ae169694c4c4d39807f512c1beaea277f
kazmi-y <yasir.h.kazmi@gmail.com>
2025-09-13 14:11:31 +0200

    Update Reebog's world.py

--- Day 6/Reebog's world.py
+++ Day 6/Reebog's world.py
@@ -2,6 +2,9 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
+
+while front_is_clear()    #to avoid the infinite loops in clear areas/trick part
+    move()
     
 while not at_goal():
     if right_is_clear():
